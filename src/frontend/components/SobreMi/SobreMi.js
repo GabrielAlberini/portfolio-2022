fa1ab6268cb0fb70cf4caedee5764cd77319f140
CuerpoCasa <gabialberini733@gmail.com>
2022-06-10 21:55:48 -0300

    V2.4

--- src/frontend/components/SobreMi/SobreMi.js
+++ src/frontend/components/SobreMi/SobreMi.js
@@ -11,15 +11,13 @@ const SobreMi = () => {
       <div className="cont-main-txt-sobre-mi">
         <div className="cont-txt-sobre-mi">
           <div className="cont-titulo-sobre-mi">
-            <h2>¿Quién es Gabriel?</h2>
+            <h2>¿Quién soy?</h2>
             <div className="cont-decorado-2">
               <img src="./assets/decorado-2.png" alt="" />
             </div>
           </div>
           <p>
-            Soy Desarrollador Web Freelancer especializado en React Js y SEO on
-            page. Trabajo de ésta manera desde hace un año realizando proyectos
-            particulares e institucionales. Continuamente en aprendizaje.
+            Mis amigos me dicen Gabo, soy Desarrollador Web especializado en React JS y SEO on page. Además del mundo de la programación y la tecnología disfruto de hablar con las personas y tratar de materializar ideas.
           </p>
         </div>
       </div>
